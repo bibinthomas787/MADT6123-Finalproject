0a376682c5ed5353a667fd9281595da468dd80cc
bibinthomas787 <bibinthomas787@gmail.com>
2020-12-03 18:06:37 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+var express=require("express"); 
+var bodyParser=require("body-parser"); 
+  
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/gfg'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+  
+var app=express() 
+  
+  
+app.use(bodyParser.json()); 
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
+  
+app.post('/sign_up', function(req,res){ 
+    var name = req.body.name; 
+    var email =req.body.email; 
+    var pass = req.body.password; 
+    var phone =req.body.phone; 
+  
+    var data = { 
+        "name": name, 
+        "email":email, 
+        "password":pass, 
+        "phone":phone 
+    } 
+db.collection('details').insertOne(data,function(err, collection){ 
+        if (err) throw err; 
+        console.log("Record inserted Successfully"); 
+              
+    }); 
+     var cursor = db.collection('details').find();
+            
+            cursor.each(function(err, item) {
+
+                if (item != null) {
+                    str = str + "    name : " + item.name + "</br>" + "email :" + item.email + ;
+                    "password :" +item.password+ "phone : " + item.phone;
+                }
+            });
+            res.send(str); 
+
+
+    //return res.redirect('signup_success.html'); 
+}) 
+  
+  
+app.get('/',function(req,res){ 
+res.set({ 
+    'Access-control-Allow-Origin': '*'
+    }); 
+return res.redirect('index.html'); 
+}).listen(3000) 
+  
+  
+console.log("server listening at port 3000"); 
